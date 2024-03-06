b0f3d7961f451bbf6c80f83af510d2050e03d335
SesifredoDev <20160643@student.chelmsford.ac.uk>
2024-03-06 09:21:03 +0000

    Target Object Animation/ Positioning

--- game.js
+++ game.js
@@ -1,3 +1,11 @@
+
+const container = document.getElementById('gameContainer');
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+
 function loadQuestion(question) {
     // create a new div element
     const newDiv = document.createElement("div");
@@ -12,12 +20,36 @@ function loadQuestion(question) {
   
     // add the newly created element and its content into the DOM
     
-    document.body.appendChild(newDiv);
+    container.appendChild(newDiv);
     return newDiv;
   }
   
+function animate(obj, level){
+    
+    let yPos;
+    let xPos;
+    let endXPos;
+    switch(level){
+        case 0:
+            yPos = "15vh";
+            break;
+        case 1:
+            yPos = "40vh";
+            break;
+        case 2:
+            yPos = "65vh";
+            break;   
+    }
+    if(level == 1){
+        xPos = "-10vw";
+        endXPos = "110vw";
+    }else{
+        xPos = "110vw";
+        endXPos = "-10vw";
+    }
 
-  
-  
-  
-  
+    console.log("xPos: " + xPos + " endXPos: " + endXPos + " yPos: " + yPos + "level" + level)
+    gsap.fromTo(obj, {y: yPos, x:xPos}, {x:endXPos, duration: 5});
+}
+
+console.log(generateQuestions())
