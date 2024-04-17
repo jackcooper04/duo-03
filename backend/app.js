cc0cff79f9d63a3cf58667359601f6c89919c33c
Jack Cooper <jc3053765@gmail.com>
2024-04-17 09:43:38 +0100

    serverUpdate

--- backend/app.js
+++ backend/app.js
@@ -80,7 +80,10 @@ app.get('/grabUserDetails/hxv8HFX3hak-aep2pqh', async (req,res,next) => {
     const foundScores = await score.find({user:req.query.id});
     const allScores = await score.find()
     .sort({score:-1})
-    .populate('user')
+    .populate({
+        path:'user',
+        match : {shown:true}
+    })
     res.json({user:foundUser,scores:foundScores,allScores:allScores})
 });
 
